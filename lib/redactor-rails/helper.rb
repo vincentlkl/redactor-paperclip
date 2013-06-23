eaef4779d05a37af6a11761a41d6b753c3d2d323
sergio1990 <sergeg1990@gmail.com>
2013-06-23 19:14:14 +0300

    -Added paperclip backend

--- lib/redactor-rails/helper.rb
+++ lib/redactor-rails/helper.rb
@@ -4,5 +4,16 @@ module RedactorRails
     def redactor_lang(lang = 'en')
       javascript_include_tag "redactor-rails/langs/#{lang}.js"
     end
+
+    class << self
+      def parameterize_filename(filename)
+        # return filename unless Ckeditor.parameterize_filenames
+
+        extension = File.extname(filename)
+        basename = filename.gsub(/#{extension}$/, "")
+        
+        [basename.parameterize('_'), extension].join.downcase
+      end
+    end
   end
 end
