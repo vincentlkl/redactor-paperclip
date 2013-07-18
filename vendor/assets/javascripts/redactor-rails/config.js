236742f9fb77a6bd9834dff3815c5d35075e70bc
Eyal Eizenberg <eyal.eizenberg+github@samanage.com>
2013-07-18 13:24:57 +0300

    Changed $ to jQuery to support prototype and jQuery envs

--- vendor/assets/javascripts/redactor-rails/config.js
+++ vendor/assets/javascripts/redactor-rails/config.js
@@ -1,12 +1,12 @@
-$(document).ready(
+jQuery(document).ready(
   function(){
-  var csrf_token = $('meta[name=csrf-token]').attr('content');
-  var csrf_param = $('meta[name=csrf-param]').attr('content');
+  var csrf_token = jQuery('meta[name=csrf-token]').attr('content');
+  var csrf_param = jQuery('meta[name=csrf-param]').attr('content');
   var params;
   if (csrf_param !== undefined && csrf_token !== undefined) {
     params = csrf_param + "=" + encodeURIComponent(csrf_token);
   }
-  $('.redactor').redactor(
+  jQuery('.redactor').redactor(
     { "imageUpload":"/redactor_rails/pictures?" + params,
       "imageGetJson":"/redactor_rails/pictures",
       "path":"/assets/redactor-rails",
