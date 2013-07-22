da732c71d73b0ee1d2d8d7f560a33b4ab15d8155
Eyal Eizenberg <eyal.eizenberg+github@samanage.com>
2013-07-22 09:20:23 +0300

    Reverted back to $ sign

--- vendor/assets/javascripts/redactor-rails/config.js
+++ vendor/assets/javascripts/redactor-rails/config.js
@@ -1,12 +1,12 @@
-jQuery(document).ready(
+$(document).ready(
   function(){
-  var csrf_token = jQuery('meta[name=csrf-token]').attr('content');
-  var csrf_param = jQuery('meta[name=csrf-param]').attr('content');
+  var csrf_token = $('meta[name=csrf-token]').attr('content');
+  var csrf_param = $('meta[name=csrf-param]').attr('content');
   var params;
   if (csrf_param !== undefined && csrf_token !== undefined) {
     params = csrf_param + "=" + encodeURIComponent(csrf_token);
   }
-  jQuery('.redactor').redactor(
+  $('.redactor').redactor(
     { "imageUpload":"/redactor_rails/pictures?" + params,
       "imageGetJson":"/redactor_rails/pictures",
       "path":"/assets/redactor-rails",
