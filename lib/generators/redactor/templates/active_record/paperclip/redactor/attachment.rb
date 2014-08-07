bee5af3c0631819648b33475b8d115bc0d1cb5a7
xana68 <alex@glyph.fr>
2014-08-07 17:08:23 +0200

    add /system on url  for paperclip model

--- lib/generators/redactor/templates/active_record/paperclip/redactor/attachment.rb
+++ lib/generators/redactor/templates/active_record/paperclip/redactor/attachment.rb
@@ -1,7 +1,7 @@
 class RedactorRails::Attachment < RedactorRails::Asset
   has_attached_file :data,
-                    url: "/redactor_rails/attachments/:id/:filename",
-                    path: ":rails_root/public/redactor_rails/attachments/:id/:filename"
+                    url: "/system/redactor_rails/attachments/:id/:filename",
+                    path: ":rails_root/public/system/redactor_rails/attachments/:id/:filename"
 
   validates_attachment_size :data, less_than: 100.megabytes
   validates_attachment :data, content_type: { content_type: /\A*\/.*\Z/ }
